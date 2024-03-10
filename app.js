da65a79eea14cd8bb619c5f9686bb217efa633f0
Bora Kol <borakol07@gmail.com>
2024-03-10 13:37:27 +0300

    updated page links

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+//Template engine
+app.set('view engine',"ejs");
+
+//Middleware
+app.use(express.static("public"));
+
+const port = 3000;
+
+//Routes
+app.get("/", (req, res) => {
+  res.status(200).render("index" , {
+    page_name : "index"
+  });
+});
+
+app.get("/about" , (req,res) =>{
+  res.status(200).render("about" , {
+    page_name: "about"
+  });
+})
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
