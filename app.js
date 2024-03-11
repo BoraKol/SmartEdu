0c19d78325a3d141e2cd791c95c38f673da65946
Bora Kol <borakol07@gmail.com>
2024-03-11 23:52:22 +0300

    models folder created and routes updated

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const pageRoute= require('./routes/pageRoute');
+const courseRoute= require('./routes/courseRoute');
+
 const app = express();
+
+//Connect DB
+
+mongoose.connect('mongodb://localhost/smartedu-db' , {
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  useFindAndModify:true,
+  useCreateIndex:true,
+}).then(() => {
+  console.log('DB connected successfully');
+});
 
 //Template engine
 app.set('view engine',"ejs");
@@ -12,6 +26,7 @@ const port = 3000;
 
 //Routes
 app.use('/' , pageRoute);
+app.use('/courses' , courseRoute);
 
 // app.get("/about" , (req,res) =>{
 //   res.status(200).render("about" , {
---
+++ controllers/courseController.js
@@ -0,0 +1,18 @@
+const Course = require("../models/Course");
+
+exports.createCourse = async (req, res) => {
+  const course = await Course.create(req.body);
+
+  try {
+    res.status(201).json({
+      // oluşturulan kursu json dosyası olarak yazdırdık
+      status: "success",
+      course,
+    });
+  } catch {
+    res.status(400).json({ // Bad request hatası gönderdik
+      status: "fail",
+      error,
+    });
+  }
+};
