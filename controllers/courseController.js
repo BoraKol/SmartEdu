e55e3028fda52a71bfdef27aa3b58b58e162e851
Bora Kol <borakol07@gmail.com>
2024-03-12 15:26:01 +0300

    course page edited and listed all courses

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,19 +1,36 @@
 const Course = require("../models/Course");
 
 exports.createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
 
   try {
+
+  const course = await Course.create(req.body);
+
     res.status(201).json({
       // oluşturulan kursu json dosyası olarak yazdırdık
       status: "success",
       course,
     });
     
-  } catch {
+  } catch(error) {
     res.status(400).json({ // Bad request hatası gönderdik
       status: "fail",
       error,
     });
   }
 };
+
+exports.getAllCourses = async(req,res) => {
+  try{
+    const courses = await Course.find();
+    res.status(200).render('courses' , {
+      courses,
+      page_name:'courses',
+    });
+  } catch(error) {
+    res.status(400).json({
+      status:'fail',
+      error,
+    })
+  }
+}
