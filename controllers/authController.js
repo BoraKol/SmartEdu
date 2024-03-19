a30846a882ed964685546f11d4072bc3ef541f04
Bora Kol <borakol07@gmail.com>
2024-03-19 21:38:53 +0300

    creating course feature is added for teacher role user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,12 @@
-const User = require('../models/User');
-
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Category = require('../models/Category');
 
 exports.createUser = async (req, res) => {
   try {
-
     const user = await User.create(req.body);
+
     res.status(201).redirect('/login');
-    
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -51,20 +50,20 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-exports.logoutUser = (req,res)=> {
+exports.logoutUser = (req, res) => {
   req.session.destroy(()=> {
     res.redirect('/');
   })
-};
-
-exports.getDashboardPage = async(req,res)=> {
-
-  const user = await User.findOne({_id:req.session.userID});
-
-  res.status(200).render('dashboard' ,{
-    page_name : "dashboard",
-    user
-  })
 }
 
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id:req.session.userID})
+  const categories = await Category.find();
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
+    categories
+  });
+};
 
